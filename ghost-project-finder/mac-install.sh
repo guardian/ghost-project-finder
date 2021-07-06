88daa020d499899aa2a1c85ee1b12efcad79cf0b
Andy Gallagher <andy.gallagher@theguardian.com>
2021-07-06 13:32:02 +0100

    update installer

--- ghost-project-finder/mac-install.sh
+++ ghost-project-finder/mac-install.sh
@@ -2,14 +2,18 @@
 
 echo Installing ghost-project-finder to /usr/local/bin and the associated plist to /Library/LaunchDaemons
 
-declare -x PLIST=com.gu.ghost-project-finder.plist
-
+declare -x DLPLIST=com.gu.ghost-project-finder-downloads.plist
+declare -x RTPLIST=com.gu.ghost-project-finder-root.plist
 install -g 0 -o 0 -m 755 ghost-project-finder /usr/local/bin
-cp ghost-project-finder.plist /Library/LaunchDaemons/$PLIST
-
+cp ghost-project-finder-downloads.plist /Library/LaunchDaemons/$DLPLIST
+cp ghost-project-finder-root.plist /Library/LaunchDaemons/$RTPLIST
 cd /Library/LaunchDaemons
-launchctl unload $PLIST || /bin/true 2>/dev/null #needed if it's been started before, but fails if it hasn't so silence the error
-launchctl load $PLIST
-launchctl start $PLIST
+launchctl unload $DLPLIST || /bin/true 2>/dev/null #needed if it's been started before, but fails if it hasn't so silence the error
+launchctl load $DLPLIST
+launchctl start $DLPLIST
+
+launchctl unload $RTPLIST || /bin/true 2>/dev/null #needed if it's been started before, but fails if it hasn't so silence the error
+launchctl load $RTPLIST
+launchctl start $RTPLIST
 
 echo Installation completed successfully
